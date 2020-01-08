9ed7c364cd82b95ad90937d411b5c5b5e917cdc5
John Rexter Flores <johnrexter.flores@gmail.com>
2020-01-09 06:55:25 +0800

    Update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title>Negosyante | Home</title>
+        <title>Sinulog Fyre | Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8">
         <meta name="keywords" content="Multi Level Marketing" />
