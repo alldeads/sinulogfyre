124c54baa4b2290456914afb2e30a1d8647585a9
John Rexter Flores <johnrexter.flores@gmail.com>
2020-01-08 21:51:32 +0800

    Create product model

--- database/migrations/2020_01_08_133748_create_products_table.php
+++ database/migrations/2020_01_08_133748_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('referral_id')->nullable();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('token');
-            $table->rememberToken();
+            $table->decimal('price');
+            $table->text('brief')->nullable();
+            $table->text('description')->nullable();
+            $table->text('avatar_first')->nullable();
+            $table->text('avatar_second')->nullable();
+            $table->text('avatar_third')->nullable();
             $table->timestamps();
         });
     }
@@ -33,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
