2a98fd38387169d3fc798021426ef5511fa65e9c
John Rexter Flores <johnrexter.flores@gmail.com>
2020-01-09 01:55:38 +0800

    Remove auth middleware

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,18 +14,8 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
 
     public function products()
     {
